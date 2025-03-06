4082cb9e06e312b3523a128b6cff37ecc9b1d61a
Shadow-Diamond <elementalpaladingaming@gmail.com>
2025-03-06 16:41:46 -0600

    Added UI

--- Assets/Scripts/Projectile.cs
+++ Assets/Scripts/Projectile.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 [RequireComponent(typeof(Rigidbody))]
 public class Projectile : MonoBehaviour {
     const int LOOKBACK_COUNT = 10;
+    static List<Projectile> PROJECTILES = new List<Projectile>();
 
     [SerializeField]
     private bool _awake = true;
@@ -22,6 +23,7 @@ public class Projectile : MonoBehaviour {
         awake = true;
         prevPos = new Vector3(1000,1000,0);
         deltas.Add(1000);
+        PROJECTILES.Add(this);
     }
 
     void FixedUpdate() {
@@ -44,6 +46,16 @@ public class Projectile : MonoBehaviour {
         if (maxDelta <= Physics.sleepThreshold) {
             awake = false;
             rigid.Sleep();
+        }
+    }
+
+    private void OnDestroy() {
+        PROJECTILES.Remove(this);
+    }
+
+    static public void DESTROY_PROJECTILES(){
+        foreach (Projectile p in PROJECTILES) {
+            Destroy(p.gameObject);
         }
     }
 }
